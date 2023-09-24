fae2b47b8cd3b1bb02e87f99c6d1ce448f6ab41d
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-09-25 00:43:20 +0700

    add grafana influxdb

--- main.go
+++ main.go
@@ -1,42 +1,22 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/redis/go-redis/v9"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
 )
 
-type User struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
+func initServer() *fiber.App {
+	app := fiber.New()
 
-const (
-	redisAdder    = "localhost:6379"
-	redisPassword = ""
-	redisDb       = 0
-	serverPort    = ":8000"
-	tokenPrefix   = "token: "
-)
-
-var (
-	redisClient *redis.Client
-)
-
-func loginHandler(c *gin.Context) {
-	var user User
-	if err := c.BindJSON(&user); err != nil {
-		return c.JSON(400, gin.H{"msg": "error"})
-	}
-}
-
-func main() {
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     redisAdder,
-		Password: redisPassword,
-		DB:       redisDb,
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		time.Sleep(time.Millisecond * 10)
+		return c.SendString("hello world")
 	})
+	return app
+}
+func main() {
+	app := initServer()
 
-	r := gin.New()
-	r.POST("/login", loginHandler())
-
+	app.Listen(":8080")
 }
---
+++ scripts/test.js
@@ -0,0 +1,15 @@
+import http from 'k6/http'
+import { check, sleep } from 'k6'
+
+export let options = {
+    vus: 5,
+    duration: '5s'
+}
+
+export default function () {
+    let res = http.get('http://host.docker.internal:8080/hello')
+
+    // check(res, { 'success login': (r) => r.status === 200 })
+
+    // sleep(0.3)
+}
